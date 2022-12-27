7e82ed1503c1af87b11d71ce26e556120f4409ac
royalty37 <patu_maori@hotmail.com>
2022-12-27 17:42:14 +1300

    Added suffix option parameter to subcommand and related function. Changed function paramater names to be more fitting with my bot.

--- src/apis/openaiService.ts
+++ src/apis/openaiService.ts
@@ -10,16 +10,29 @@ class OpenAIService {
     this.openai = new OpenAIApi(config);
   }
 
-  public async createCompletion(prompt: string): Promise<string | undefined> {
+  public async createCompletion(input: string, suffix?: string): Promise<string | undefined> {
     const response = await this.openai.createCompletion({
       model: "text-davinci-003",
       max_tokens: 4000,
-      prompt,
+      prompt: input,
+      suffix,
       temperature: 0.1,
     });
 
     return response.data.choices[0].text;
   }
+
+  // public async createEdit(prompt: string, edit: string): Promise<string | undefined> {
+  //   const response = await this.openai.createEdit({
+  //     model: "text-davinci-003",
+  //     max_tokens: 4000,
+  //     prompt,
+  //     temperature: 0.1,
+  //     edit,
+  //   });
+
+  //   return response.data.choices[0].text;
+  // }
 }
 
 export default OpenAIService;
--- src/commands/openai.ts
+++ src/commands/openai.ts
@@ -10,7 +10,48 @@ const textSubcommand = (sc: SlashCommandSubcommandBuilder) =>
     .setDescription("Responds to user input with generated text (the sky is the limit!)")
     .addStringOption((option) =>
       option.setName("input").setDescription("Input used to generate AI text response").setRequired(INPUT_REQUIRED)
+    )
+    .addStringOption((option) =>
+      option.setName("suffix").setDescription("Suffix to add to the end of the generated text")
     );
+
+const handleTextSubcommand = async (interaction: ChatInputCommandInteraction) => {
+  try {
+    await interaction.deferReply();
+
+    const input = interaction.options.getString("input", INPUT_REQUIRED);
+    const suffix = interaction.options.getString("suffix") ?? undefined;
+
+    const res = await new OpenAIService().createCompletion(input, suffix);
+
+    console.log(`*** OPENAI RES:${res}\n\nEND OF RES ***`);
+
+    if (res) {
+      const reply = `**INPUT:**\n${input}\n\n${suffix ? `**SUFFIX:**\n${suffix}\n\n` : ""}**OPENAI RESPONSE:**${res}${
+        suffix ? ` ${suffix}` : ""
+      }`;
+
+      if (reply.length > 2000) {
+        for (let i = 0; i < reply.length; i += 2000) {
+          const toSend = reply.substring(i, Math.min(reply.length, i + 2000));
+
+          if (i === 0) {
+            await interaction.editReply(toSend);
+          } else {
+            await interaction.followUp(toSend);
+          }
+        }
+      } else {
+        await interaction.editReply(reply);
+      }
+    } else {
+      await interaction.editReply("Something went wrong. Please try again.");
+    }
+  } catch (e) {
+    console.error("OPEN AI COMMAND EXECEPTION: " + e);
+    await interaction.editReply("Something went wrong. Please try again.");
+  }
+};
 
 const data = new SlashCommandBuilder()
   .setName("openai")
@@ -19,37 +60,7 @@ const data = new SlashCommandBuilder()
 
 const execute = async (interaction: ChatInputCommandInteraction) => {
   if (interaction.options.getSubcommand() === "text") {
-    try {
-      await interaction.deferReply();
-
-      const text = interaction.options.getString("input", INPUT_REQUIRED);
-      const res = await new OpenAIService().createCompletion(text);
-
-      console.log(`*** OPENAI RES:${res}\n\nEND OF RES ***`);
-
-      if (res) {
-        const reply = `**INPUT:**\n${text}\n\n**OPENAI RESPONSE:**${res}`;
-
-        if (reply.length > 2000) {
-          for (let i = 0; i < reply.length; i += 2000) {
-            const toSend = reply.substring(i, Math.min(reply.length, i + 2000));
-
-            if (i === 0) {
-              await interaction.editReply(toSend);
-            } else {
-              await interaction.followUp(toSend);
-            }
-          }
-        } else {
-          await interaction.editReply(reply);
-        }
-      } else {
-        await interaction.editReply("Something went wrong. Please try again.");
-      }
-    } catch (e) {
-      console.error("OPEN AI COMMAND EXECEPTION: " + e);
-      await interaction.editReply("Something went wrong. Please try again.");
-    }
+    handleTextSubcommand(interaction);
   }
 };
 
