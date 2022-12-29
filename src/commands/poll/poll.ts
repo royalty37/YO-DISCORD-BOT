112c72981b1e18d6f414905aeb709efd43fcf105
royalty37 <patu_maori@hotmail.com>
2022-12-30 05:44:42 +1300

    Functional poll system. Also can set multivote. TODO: Fix filter function as it is written badly as I was debugging. Comment code.

--- src/commands/poll/poll.ts
+++ src/commands/poll/poll.ts
@@ -4,15 +4,19 @@ import {
   SlashCommandStringOption,
   EmbedBuilder,
   Colors,
+  MessageReaction,
+  User,
 } from "discord.js";
 import Command from "../../types/Command";
 
 const EMOJI_NUMBERS = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣", "🔟"];
 
-const QUESTION_REQUIRED = true;
-const NO_OF_OPTIONS = 10;
 const QUESTION_OPTION_NAME = "question";
-const OPTION_NAME_PREFIX = "option";
+const QUESTION_REQUIRED = true;
+const ALLOW_MULTIVOTE_OPTION_NAME = "allow-multiple-votes";
+const MULTIVOTE_REQUIRED = true;
+const NO_OF_OPTIONS = 10;
+const OPTION_NAME_PREFIX = "option-";
 
 // Poll command SlashCommandBuilder
 const data = new SlashCommandBuilder()
@@ -23,6 +27,9 @@ const data = new SlashCommandBuilder()
       .setName(QUESTION_OPTION_NAME)
       .setDescription("Question to ask on poll. Must have AT LEAST 2 options.")
       .setRequired(QUESTION_REQUIRED)
+  )
+  .addBooleanOption((option) =>
+    option.setName(ALLOW_MULTIVOTE_OPTION_NAME).setDescription("Allow multiple votes").setRequired(MULTIVOTE_REQUIRED)
   );
 
 // Add options to poll command
@@ -40,6 +47,7 @@ const execute = async (interaction: ChatInputCommandInteraction) => {
   await interaction.deferReply();
 
   const question = interaction.options.getString(QUESTION_OPTION_NAME, QUESTION_REQUIRED);
+  const allowMultiVote = interaction.options.getBoolean(ALLOW_MULTIVOTE_OPTION_NAME, MULTIVOTE_REQUIRED);
   const options: string[] = [];
 
   for (let i = 1; i <= NO_OF_OPTIONS; i++) {
@@ -49,18 +57,78 @@ const execute = async (interaction: ChatInputCommandInteraction) => {
     }
   }
 
-  const pollEmbedDescription = options.map((o, i) => EMOJI_NUMBERS[i] + " - " + o).join("\n");
+  console.log("OPTIONS LENGTH", options.length);
+
+  const votes = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  let userVoted: string[] = [];
+
+  const generateDescription = () => {
+    const generatePercentage = (index: number): string => {
+      const percentage = (votes[index] / votes.reduce((a, b) => a + b, 0)) * 100 || 0;
+
+      if (percentage.toString().includes(".") && percentage.toString().split(".")[1].length > 2) {
+        return percentage.toFixed(2);
+      }
+
+      return percentage.toString();
+    };
+
+    return options.map((o, i) => `${EMOJI_NUMBERS[i]} - ${o} | ${votes[i]} (${generatePercentage(i)}%)`).join("\n");
+  };
 
   const pollEmbed = new EmbedBuilder()
     .setTitle(question)
-    .setDescription(options.join("\n"))
+    .setDescription(generateDescription())
     .setColor(Colors.Red)
     .setThumbnail(interaction.user.avatarURL())
     .setFooter({
       text: `Poll created by ${interaction.user.username}`,
     });
 
-  await interaction.editReply({ embeds: [pollEmbed] });
+  const message = await interaction.editReply({ embeds: [pollEmbed] });
+
+  const filter = (reaction: MessageReaction, user: User) => {
+    if (EMOJI_NUMBERS.indexOf(reaction.emoji.name ?? "") >= options.length) {
+      return false;
+    }
+    console.log("EMOJI NUMBERS INDEX", EMOJI_NUMBERS.indexOf(reaction.emoji.name ?? ""));
+    console.log("OPTIONS LENGTH", options.length);
+    return EMOJI_NUMBERS.includes(reaction.emoji.name ?? "") &&
+      EMOJI_NUMBERS.indexOf(reaction.emoji.name ?? "") < options.length &&
+      !allowMultiVote
+      ? !userVoted.includes(user.id)
+      : true;
+  };
+
+  const collector = message.createReactionCollector({
+    filter,
+    time: 60000 * 60 * 2,
+    dispose: true,
+  });
+
+  collector.on("collect", (reaction: MessageReaction, user: User) => {
+    const index = EMOJI_NUMBERS.indexOf(reaction.emoji.name ?? "");
+    votes[index]++;
+
+    if (!allowMultiVote) {
+      userVoted.push(user.id);
+    }
+
+    pollEmbed.setDescription(generateDescription());
+    message.edit({ embeds: [pollEmbed] });
+  });
+
+  collector.on("remove", (reaction: MessageReaction, user: User) => {
+    const index = EMOJI_NUMBERS.indexOf(reaction.emoji.name ?? "");
+    votes[index]--;
+
+    if (!allowMultiVote) {
+      userVoted = userVoted.filter((id) => id !== user.id);
+    }
+
+    pollEmbed.setDescription(generateDescription());
+    message.edit({ embeds: [pollEmbed] });
+  });
 };
 
 const pollCommand: Command = {
