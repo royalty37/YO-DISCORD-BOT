30254ca3ba6906b992840eea20f34ccfd74278d0
royalty37 <patu_maori@hotmail.com>
2022-12-31 01:57:11 +1300

    Bunch of changes. Added a duration required duration option. Added a getFooter function for generating the footer text with an updated remaining duration (or no duration). Added a nifty little recursive updateDuration function that uses setTimeout to update duration and resend poll message every minute. Pretty pleased with it admittedly. Fixed an issue with removing invalid votes breaking vote count due to remove listener, added a little roundabout solution that I do not like but it works... Also added an on end listener that will print 'Poll has ended blah blah' with a nice little 'option x is the winner' message.

--- src/commands/poll/poll.ts
+++ src/commands/poll/poll.ts
@@ -7,6 +7,7 @@ import {
   MessageReaction,
   User,
   SlashCommandBooleanOption,
+  SlashCommandNumberOption,
 } from "discord.js";
 import Command from "../../types/Command";
 
@@ -20,6 +21,8 @@ const QUESTION_OPTION_NAME = "question";
 const QUESTION_REQUIRED = true;
 const ALLOW_MULTIVOTE_OPTION_NAME = "allow-multiple-votes";
 const MULTIVOTE_REQUIRED = true;
+const DURATION_OPTION_NAME = "duration";
+const DURATION_REQUIRED = true;
 const NO_OF_OPTIONS = 10;
 const OPTION_NAME_PREFIX = "option-";
 
@@ -35,6 +38,9 @@ const data = new SlashCommandBuilder()
   )
   .addBooleanOption((option: SlashCommandBooleanOption) =>
     option.setName(ALLOW_MULTIVOTE_OPTION_NAME).setDescription("Allow multiple votes").setRequired(MULTIVOTE_REQUIRED)
+  )
+  .addNumberOption((option: SlashCommandNumberOption) =>
+    option.setName(DURATION_OPTION_NAME).setDescription("Poll duration (in minutes)").setRequired(DURATION_REQUIRED)
   );
 
 // Add options to poll command
@@ -52,9 +58,10 @@ for (let i = 1; i <= NO_OF_OPTIONS; i++) {
 const execute = async (interaction: ChatInputCommandInteraction) => {
   await interaction.deferReply();
 
-  // Get Question and Allow-Multi-Vote
+  // Get Question, Allow-Multi-Vote and Duration from options
   const question = interaction.options.getString(QUESTION_OPTION_NAME, QUESTION_REQUIRED);
   const allowMultiVote = interaction.options.getBoolean(ALLOW_MULTIVOTE_OPTION_NAME, MULTIVOTE_REQUIRED);
+  const duration = interaction.options.getNumber(DURATION_OPTION_NAME, DURATION_REQUIRED);
 
   // Loop through and get possible options
   const options: string[] = [];
@@ -96,6 +103,16 @@ const execute = async (interaction: ChatInputCommandInteraction) => {
       .join("\n\n");
   };
 
+  // Function to generate footer text for poll embed, if no duration is provided, then remaining duration is not shown (used for poll end)
+  const getFooterText = (remainingDurationParam?: number): string =>
+    `Poll created by ${interaction.user.username}${
+      remainingDurationParam
+        ? `\n\nPoll will end in approximately ${
+            remainingDurationParam > 1 ? `${remainingDurationParam} minutes.` : `${remainingDurationParam} minute.`
+          }`
+        : ""
+    }`;
+
   // Create initial poll embed - No votes yet
   const pollEmbed = new EmbedBuilder()
     .setTitle(question)
@@ -103,11 +120,28 @@ const execute = async (interaction: ChatInputCommandInteraction) => {
     .setColor(Colors.Red)
     .setThumbnail(interaction.user.avatarURL())
     .setFooter({
-      text: `Poll created by ${interaction.user.username}`,
+      text: getFooterText(duration),
     });
 
   // Edit reply to include poll embed and pull out message to edit after collecting reactions
   const message = await interaction.editReply({ embeds: [pollEmbed] });
+
+  // Recursive function edit embed every minute to show remaining duration
+  const updateDuration = async (duration: number) => {
+    setTimeout(async () => {
+      pollEmbed.setFooter({
+        text: getFooterText(duration),
+      });
+      await message.edit({ embeds: [pollEmbed] });
+
+      if (duration > 0) {
+        updateDuration(duration - 1);
+      }
+    }, 60000);
+  };
+
+  // Initial call
+  updateDuration(duration);
 
   for (let i = 0; i < options.length; i++) {
     // Add bot reaction to message for each option
@@ -117,8 +151,8 @@ const execute = async (interaction: ChatInputCommandInteraction) => {
   // Create reaction collector - no filter (manually handle in collect listener), 2 hour time limit, dispose = true (allow remove listener)
   const collector = message.createReactionCollector({
     filter: (_, user: User) => !user.bot,
-    // More readable than 720k ms, 60,000ms = 1 minute * 60 = 1 hour * 2 = 2 hours
-    time: 60000 * 60 * 2,
+    // 60,000 ms (a minute) * duration (in minutes)
+    time: 60000 * duration,
     dispose: true,
   });
 
@@ -134,6 +168,9 @@ const execute = async (interaction: ChatInputCommandInteraction) => {
       return void reaction.remove();
     }
 
+    // Get index of option that user voted for early to avoid a weird remove listener bug
+    const index = EMOJI_NUMBERS.indexOf(reaction.emoji.name ?? "");
+
     // If not a multi-vote poll, check if user has already voted, if so, remove reaction and early return
     if (!allowMultiVote) {
       let userHasVoted = false;
@@ -148,15 +185,17 @@ const execute = async (interaction: ChatInputCommandInteraction) => {
         userHasVoted = !!existingReaction.users.cache.get(user.id);
       });
 
-      // If user has already voted, remove reaction and early return
+      // If user has already voted, remove that users reaction and early return
       if (userHasVoted) {
-        return void reaction.remove();
+        // Calling reaction.users.remove() will trigger the remove listener, so we need to manually increment the vote count before the remove listener decrements it
+        // A slight roundabout solution but not necessarily non performant - Would prefer to block the remove listener or early return in the remove listener but unsure if it's doable
+        votes[index]++;
+        return void reaction.users.remove(user);
       }
     }
 
     // If everything is all good, register the vote
     // Get index of reaction emoji in EMOJI_NUMBERS array and increment vote count for that index
-    const index = EMOJI_NUMBERS.indexOf(reaction.emoji.name ?? "");
     votes[index]++;
 
     // Update poll embed description and edit message
@@ -165,13 +204,46 @@ const execute = async (interaction: ChatInputCommandInteraction) => {
   });
 
   // On Remove listener
-  collector.on("remove", (reaction: MessageReaction) => {
+  collector.on("remove", (reaction: MessageReaction, user: User) => {
     // Get index of reaction emoji in EMOJI_NUMBERS array and decrement vote count for that index
     const index = EMOJI_NUMBERS.indexOf(reaction.emoji.name ?? "");
     votes[index]--;
 
     // Update poll embed description and edit message
     pollEmbed.setDescription(generateDescription());
+    message.edit({ embeds: [pollEmbed] });
+  });
+
+  // On End listener
+  collector.on("end", () => {
+    // Filter options array to only include options with the highest vote count
+    const maxVotes = Math.max(...votes);
+    const winners = options.filter((_, i) => votes[i] === maxVotes);
+
+    // If no votes, write a sad no votes to description
+    if (!maxVotes) {
+      pollEmbed.setDescription(
+        generateDescription() + "\n\n**Poll has ended**\n\nUnfortunately, no votes were cast. 😔😔😔"
+      );
+    } else if (winners.length === 1) {
+      // If there is only one winner, add winner to description
+      pollEmbed.setDescription(
+        generateDescription() +
+          `\n\n**Poll has ended**\n\n👑 **${winners[0]}** 👑 is the winner with ${
+            maxVotes > 1 ? `${maxVotes} votes` : `${maxVotes} vote`
+          }.`
+      );
+    } else {
+      // If there are multiple winners, add winners to description
+      pollEmbed.setDescription(
+        generateDescription() +
+          `\n\n**Poll has ended**\n\nWinners are:\n\n ${winners.map((w) => `👑 **${w}** 👑`).join("\n")}\n\n with ${
+            maxVotes > 1 ? `${maxVotes} votes` : `${maxVotes} vote`
+          } each.`
+      );
+    }
+
+    pollEmbed.setFooter({ text: getFooterText() });
     message.edit({ embeds: [pollEmbed] });
   });
 };
