615350114b5cfc7c3c397b4639c497e97bde9cd6
royalty37 <patu_maori@hotmail.com>
2023-01-02 02:24:45 +1300

    I think the bumboy feature is actually finished now.

--- src/commands/bumboy/bumboy.ts
+++ src/commands/bumboy/bumboy.ts
@@ -1,14 +1,13 @@
 import {
   SlashCommandBuilder,
   ChatInputCommandInteraction,
-  SlashCommandStringOption,
   EmbedBuilder,
   Colors,
   MessageReaction,
   User,
 } from "discord.js";
 import Command from "../../types/Command";
-import { getUniqueRandomEmojismojis } from "../../utils/emojiUtils/emojiUtils";
+import { getUniqueRandomEmojis } from "../../utils/emojiUtils/emojiUtils";
 
 // Feature to excite the boys - the YOZA BUMBOY feature
 
@@ -30,7 +29,8 @@ const DURATION = 60000 * NO_OF_MINUTES;
 const NO_OF_OPTIONS = 20;
 
 // Command cooldown only allows one BUMBOY poll per day
-const onCooldown = false;
+let onCooldown = false;
+let bumboys: string[] = [];
 
 // Bumboy command SlashCommandBuilder
 const data = new SlashCommandBuilder()
@@ -39,16 +39,30 @@ const data = new SlashCommandBuilder()
 
 // Bumboy command execute function
 const execute = async (interaction: ChatInputCommandInteraction) => {
+  if (onCooldown) {
+    return void (await interaction.reply(
+      "BUMBOY poll can only be run once every 24 hours.\n\nCurrent BUMBOYS are:\n\n" +
+        bumboys.map((b) => `💩 **${b}** 💩`).join("\n\n") +
+        "\n\nTry again later."
+    ));
+  }
+
+  // Set onCooldown to true to prevent multiple polls being run at once
+  onCooldown = true;
+
+  // Set onCooldown to false after 24 hours
+  setTimeout(() => {
+    onCooldown = false;
+  }, 1000 * 60 * 60 * 24);
+
   await interaction.deferReply();
 
   // Fetch all members then fetch the Vice Plus role to get all members with that role
   await interaction.guild?.members.fetch();
   const vicePlusRole = await interaction.guild?.roles.fetch(VICE_PLUS_ROLE_ID);
-  const bumboyRole = interaction.guild?.roles.fetch(BUMBOY_ROLE_ID);
+  const bumboyRole = await interaction.guild?.roles.fetch(BUMBOY_ROLE_ID);
 
-  console.log("ROLES: ", interaction.guild?.roles);
-
-  if (!vicePlusRole?.members) {
+  if (!vicePlusRole?.members || !bumboyRole) {
     return void (await interaction.editReply("Something went wrong. Please try again."));
   }
 
@@ -61,12 +75,7 @@ const execute = async (interaction: ChatInputCommandInteraction) => {
     .filter((m) => !options.includes(m));
 
   // Get a random emoji for each option - VICE PLUS
-  const emojis = getUniqueRandomEmojismojis(options.length);
-
-  // Programatically get emojis for each option
-  interaction.guild?.emojis.cache.forEach((e) => {
-    console.log("EMOJI NAME: ", e.name);
-  });
+  const emojis = getUniqueRandomEmojis(options.length);
 
   // Programatically generate votes array corresponding to number of VICE PLUS members included in the poll
   const votes: number[] = [];
@@ -88,18 +97,26 @@ const execute = async (interaction: ChatInputCommandInteraction) => {
       return percentage.toString();
     };
 
-    // Sort options by vote count, then map to generate information about each option, and join each option with a double new line
-    // Spread options into new array to avoid mutating original array on sort
-    return [...options]
-      .sort((a, b) => votes[options.indexOf(b)] - votes[options.indexOf(a)])
-      .map((o) => {
-        // Generate green emoji bar for each option showing vote count
-        const votesEmojis = VOTE_EMOJI.repeat(votes[options.indexOf(o)]);
-        return `${emojis[options.indexOf(o)]} ${o}\n ${votesEmojis ? `${votesEmojis} | ` : ""}${
-          votes[options.indexOf(o)]
-        } (${generatePercentage(options.indexOf(o))}%)`;
-      })
-      .join("\n\n");
+    // Generate description for poll embed, include members who were not randomly selected for the poll
+    return (
+      "The BUMBOY poll is a poll to determine who will be demoted to BUMBOY and have their nickname changed for 24 hours. This doesn't revoke any permissions (except for the ability to change your nickname), but the BUMBOY must suffer the embarrassment of being the BUMBOY.\n\n" +
+      "This poll is a non multi vote poll, which means you can only cast a single vote. To change your vote, remove your existing vote (reaction) and cast a new vote.\n\nOnly members of the Vice Plus role may participate.\n\nThe BUMBOY poll can only be run once every 24 hours.\n\n" +
+      "Luckily, the following members are exempt from todays YOZA Bumboy vote (Discord only allows a maximum of 20 reacts on a message):\n\n" +
+      nonIncludedMembers.map((m) => `🍀 ${m} 🍀`).join("\n\n") +
+      "\n\nMembers included in todays BUMBOY poll are:\n\n" +
+      // Sort options by vote count, then map to generate information about each option, and join each option with a double new line
+      // Spread options into new array to avoid mutating original array on sort
+      [...options]
+        .sort((a, b) => votes[options.indexOf(b)] - votes[options.indexOf(a)])
+        .map((o) => {
+          // Generate green emoji bar for each option showing vote count
+          const votesEmojis = VOTE_EMOJI.repeat(votes[options.indexOf(o)]);
+          return `${emojis[options.indexOf(o)]} ${o}\n ${votesEmojis ? `${votesEmojis} | ` : ""}${
+            votes[options.indexOf(o)]
+          } (${generatePercentage(options.indexOf(o))}%)`;
+        })
+        .join("\n\n")
+    );
   };
 
   const getFooterText = (remainingDurationParam?: number): string =>
@@ -118,7 +135,7 @@ const execute = async (interaction: ChatInputCommandInteraction) => {
     .setColor(Colors.Red)
     .setThumbnail(interaction.user.avatarURL())
     .setFooter({
-      text: getFooterText(DURATION),
+      text: getFooterText(NO_OF_MINUTES),
     });
 
   // Edit reply to include poll embed and pull out message to edit after collecting reactions
@@ -160,6 +177,14 @@ const execute = async (interaction: ChatInputCommandInteraction) => {
 
     // Get index of option that user voted for early to avoid a weird remove listener bug
     const index = emojis.indexOf(reaction.emoji.name ?? "");
+
+    // If user is not a vice plus, remove reaction and early return
+    if (!vicePlusRole.members.has(user.id)) {
+      // Calling reaction.users.remove() will trigger the remove listener, so we need to manually increment the vote count before the remove listener decrements it
+      // A slight roundabout solution but not necessarily non performant - Would prefer to block the remove listener or early return in the remove listener but unsure if it's doable
+      votes[index]++;
+      return void reaction.users.remove(user);
+    }
 
     // Check if user has already voted, if so, remove reaction and early return
     let userHasVoted = false;
@@ -203,36 +228,65 @@ const execute = async (interaction: ChatInputCommandInteraction) => {
   });
 
   // On End listener
-  collector.on("end", () => {
+  collector.on("end", async () => {
     // Filter options array to only include options with the highest vote count
     const maxVotes = Math.max(...votes);
-    const winners = options.filter((_, i) => votes[i] === maxVotes);
+    bumboys = options.filter((_, i) => votes[i] === maxVotes);
 
-    // If no votes, write a sad no votes to description
+    // If no votes, write a sad no votes message to description
     if (!maxVotes) {
       pollEmbed.setDescription(
-        generateDescription() + "\n\n**Poll has ended**\n\nUnfortunately, no votes were cast. 😔😔😔"
+        generateDescription() + "\n\n**BUMBOY poll has ended**\n\nUnfortunately, no votes were cast. 😔😔😔"
       );
-    } else if (winners.length === 1) {
+    } else if (bumboys.length === 1) {
       // If there is only one winner, add winner to description
       pollEmbed.setDescription(
         generateDescription() +
-          `\n\n**BUMBOY Poll has ended**\n\n👑 **${winners[0]}** 👑 is the winner with ${
-            maxVotes > 1 ? `${maxVotes} votes` : `${maxVotes} vote`
+          `\n\n**BUMBOY Poll has ended**\n\n💩 **${bumboys[0]}** 💩 is the todays BUMBOY with ${maxVotes} ${
+            maxVotes > 1 ? `votes` : `vote`
           }.`
       );
     } else {
       // If there are multiple winners, add winners to description
       pollEmbed.setDescription(
         generateDescription() +
-          `\n\n**Poll has ended**\n\nWinners are:\n\n ${winners
-            .map((w) => `👑 **${w}** 👑`)
+          `\n\n**BUMBOY Poll has ended**\n\nTodays BUMBOYS are:\n\n ${bumboys
+            .map((w) => `💩 **${w}** 💩`)
             .join("\n")}\n\n with ${maxVotes} ${maxVotes > 1 ? `votes` : `vote`} each.`
       );
     }
 
     pollEmbed.setFooter({ text: getFooterText() });
     message.edit({ embeds: [pollEmbed] });
+
+    // Store old nicknames so I can reset them later
+    const oldNicknames: string[] = [];
+
+    // Set roles and change nicknames for bumboys
+    for (let i = 0; i < bumboys.length; i++) {
+      const member = interaction.guild?.members.cache.find((m) => m.user.username === bumboys[i]);
+      if (member) {
+        member.roles.add(bumboyRole);
+        member.roles.remove(vicePlusRole);
+        oldNicknames.push(member.nickname ?? member.user.username);
+        member.setNickname(`💩 ${bumboys[i]} 💩`);
+      }
+    }
+
+    // Set timeout to promote bumboys back to Vice Plus after 24 hours and reset their nicknames
+    setTimeout(() => {
+      for (let i = 0; i < bumboys.length; i++) {
+        const member = interaction.guild?.members.cache.find((m) => m.user.username === bumboys[i]);
+        if (member) {
+          member.roles.remove(bumboyRole);
+          member.roles.add(vicePlusRole);
+          member.setNickname(oldNicknames[i]);
+        }
+      }
+
+      // Set bumboys array to empty ready for next poll
+      bumboys = [];
+    }, 60000 * 24);
   });
 };
 
