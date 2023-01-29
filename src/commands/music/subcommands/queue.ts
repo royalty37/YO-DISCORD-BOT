6e2785324e11a0bbf4c4478d34c647193e45c17f
royalty37 <patu_maori@hotmail.com>
2023-01-29 23:31:03 +1300

    Queue subcommand finished - 10 songs per page with next and previous buttons. Finished for now but might make changes later

--- src/commands/music/subcommands/queue.ts
+++ src/commands/music/subcommands/queue.ts
@@ -16,138 +16,93 @@ export const queueSubcommand = (sc: SlashCommandSubcommandBuilder) =>
 
 // Music resume subcommand execution
 export const handleQueueSubcommand = async (interaction: Interaction<ChatInputCommandInteraction>) => {
+  // If no guild ID, return
   if (!interaction.guildId) {
     console.log("*** MUSIC RESUME SUBCOMMAND - NO GUILD ID");
     return void interaction.reply("Something went wrong. Please try again.");
   }
 
+  // If no channel, return
   if (!interaction.channel) {
     console.log("*** MUSIC RESUME SUBCOMMAND - NO CHANNEL");
     return void interaction.reply("Something went wrong. Please try again.");
   }
 
   // Get DisTube queue from client from interaction
-  const queue = interaction.client.distube.getQueue(interaction.guildId ?? "");
+  const queue = interaction.client.distube.getQueue(interaction.guildId);
 
+  // If no queue, return
   if (!queue) {
     console.log("*** MUSIC RESUME SUBCOMMAND - NO QUEUE");
     return void interaction.reply("❌ | No music is being played!");
   }
 
+  // Array of embed descriptions for separate pages
   const embedDescriptions: string[] = [];
 
+  // Loop through all songs and build up embed descriptions array with 10 songs per page
   let currentEmbedDescription = "";
   queue.songs.forEach((song, index) => {
     const songToAppend = `${index + 1}. ${song.name}${index === 0 ? " (currently playing)" : ""}\n\n`;
-    if (currentEmbedDescription.length + songToAppend.length > 2048) {
+    if (index !== 0 && index % 10 === 0) {
       embedDescriptions.push(currentEmbedDescription);
       currentEmbedDescription = songToAppend;
     } else {
-      currentEmbedDescription = currentEmbedDescription.concat(songToAppend);
+      currentEmbedDescription += songToAppend;
       if (index === queue.songs.length - 1) {
         embedDescriptions.push(currentEmbedDescription);
       }
     }
   });
 
+  // Current page index - starts at 0
   let currentIndex = 0;
 
-  const message = await interaction.reply({
+  // Function to generate/regenerate reply object when pressing next/previous buttons
+  const generateReplyObject = () => ({
     embeds: [
       new EmbedBuilder()
         .setColor("Random")
         .setTitle("🎶 | Current queue:")
-        .setThumbnail(queue.songs[0].thumbnail ?? null)
+        .setThumbnail(queue.songs[currentIndex].thumbnail ?? null)
         .setDescription(embedDescriptions[currentIndex])
-        .setFooter({ text: `Page 1 of ${currentIndex + 1}` }),
+        .setFooter({ text: `Page ${currentIndex + 1} of ${embedDescriptions.length}` }),
     ],
     components: [
       new ActionRowBuilder<ButtonBuilder>().addComponents([
-        new ButtonBuilder().setCustomId("queue-prev-page").setLabel("Previous").setStyle(ButtonStyle.Primary),
-        // .setDisabled(currentIndex <= 0),
-        new ButtonBuilder().setCustomId("queue-next-next").setLabel("Next").setStyle(ButtonStyle.Primary),
-        // .setDisabled(currentIndex >= embedDescriptions.length - 1),
+        new ButtonBuilder()
+          .setCustomId("queue-prev-page")
+          .setLabel("Previous")
+          .setStyle(ButtonStyle.Primary)
+          .setDisabled(currentIndex <= 0),
+        new ButtonBuilder()
+          .setCustomId("queue-next-page")
+          .setLabel("Next")
+          .setStyle(ButtonStyle.Primary)
+          .setDisabled(currentIndex >= embedDescriptions.length - 1),
       ]),
     ],
   });
 
+  // Reply with first page and get message object
+  const message = await interaction.reply(generateReplyObject());
+
+  // Create message component collector
   const collector = message.createMessageComponentCollector({
     componentType: ComponentType.Button,
-    filter: (i) => i.customId === "queue-prev-page" || i.customId === "queue-next-page",
-    time: 1000 * 60 * 5,
+    time: 1000 * 60 * 10, // 10 minutes
   });
 
+  // On collect increment/decrement current index and regenerate reply object
   collector.on("collect", async (interaction) => {
-    console.log("COLLECTOR HIT");
     if (interaction.customId === "queue-prev-page") {
       console.log("*** MUSIC QUEUE SUBCOMMAND - PREV PAGE");
       currentIndex--;
-      await interaction.update({
-        embeds: [
-          new EmbedBuilder()
-            .setColor("Random")
-            .setTitle("🎶 | Current queue:")
-            .setThumbnail(queue.songs[0].thumbnail ?? null)
-            .setDescription(embedDescriptions[currentIndex])
-            .setFooter({ text: `Page ${currentIndex + 1} of ${embedDescriptions.length}` }),
-        ],
-        components: [
-          new ActionRowBuilder<ButtonBuilder>().addComponents([
-            new ButtonBuilder().setCustomId("queue-prev-page").setLabel("Previous").setStyle(ButtonStyle.Primary),
-            // .setDisabled(currentIndex === 0),
-            new ButtonBuilder().setCustomId("queue-next-next").setLabel("Next").setStyle(ButtonStyle.Primary),
-            // .setDisabled(currentIndex === embedDescriptions.length - 1),
-          ]),
-        ],
-      });
     } else if (interaction.customId === "queue-next-page") {
       console.log("*** MUSIC QUEUE SUBCOMMAND - NEXT PAGE");
       currentIndex++;
-      await interaction.update({
-        embeds: [
-          new EmbedBuilder()
-            .setColor("Random")
-            .setTitle("🎶 | Current queue:")
-            .setThumbnail(queue.songs[0].thumbnail ?? null)
-            .setDescription(embedDescriptions[currentIndex])
-            .setFooter({ text: `Page ${currentIndex + 1} of ${embedDescriptions.length}` }),
-        ],
-        components: [
-          new ActionRowBuilder<ButtonBuilder>().addComponents([
-            new ButtonBuilder().setCustomId("queue-prev-page").setLabel("Previous").setStyle(ButtonStyle.Primary),
-            // .setDisabled(currentIndex === 0),
-            new ButtonBuilder().setCustomId("queue-next-next").setLabel("Next").setStyle(ButtonStyle.Primary),
-            // .setDisabled(currentIndex === embedDescriptions.length - 1),
-          ]),
-        ],
-      });
     }
-  });
 
-  // for (let i = 0; i < embedDescriptions.length; i++) {
-  //   if (i === 0) {
-  //     console.log("*** MUSIC QUEUE SUBCOMMAND - SENDING QUEUE PAGE 1 (i === 0)");
-  //     await interaction.reply({
-  //       embeds: [
-  //         new EmbedBuilder()
-  //           .setColor("Random")
-  //           .setTitle("🎶 | Current queue:")
-  //           .setThumbnail(queue.songs[0].thumbnail ?? null)
-  //           .setDescription(embedDescriptions[i])
-  //           .setFooter({ text: `Page 1 of ${embedDescriptions.length}` }),
-  //       ],
-  //     });
-  //   } else {
-  //     console.log(`*** MUSIC QUEUE SUBCOMMAND - SENDING QUEUE PAGE ${i + 1}`);
-  //     await interaction.followUp({
-  //       embeds: [
-  //         new EmbedBuilder()
-  //           .setColor("Random")
-  //           .setTitle("🎶 | Current queue:")
-  //           .setDescription(embedDescriptions[i])
-  //           .setFooter({ text: `Page ${i + 1} of ${embedDescriptions.length}` }),
-  //       ],
-  //     });
-  //   }
-  // }
+    await interaction.update(generateReplyObject());
+  });
 };
